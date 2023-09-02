1682ff870032b2240fff83ad29cf26ef534d318e
魔羯毅 <102799176+design61@users.noreply.github.com>
2023-09-02 18:49:40 +0800

    Update interface.py

--- scripts/interface.py
+++ scripts/interface.py
@@ -1,12 +1,6 @@
-import gradio as gr
 from modules import scripts
 
 
 class Script(scripts.Script):
     def title(self):
         return "QR checkBTN"
-
-    def after_component(self, component,**kwargs):
-        if kwargs.get("elem_id")=="extras_tab":
-            basic_send_button=gr.Button("检查二维码可否识别")
-            basic_send_button.click(None,[],None,_js="()=>qrCheckImage(this)")
